cd17c22a4b855792190b62a9cd890c3031706b69
ajn123 <ajn123@vt.edu>
2015-12-01 06:02:23 -0500

    Initial Commit

---
+++ ToDoListApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by Mark Norton on 12/1/15.
+//  Copyright © 2015 Alex Norton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
